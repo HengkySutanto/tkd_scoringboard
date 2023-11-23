ca6d03583dfdf75082b1c7e9ec72c905b93d4bed
HengkyOcistock <114140439+HengkyOcistock@users.noreply.github.com>
2023-11-23 12:22:57 +0700

    fiuuhhhh

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from '@/components/general/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,12 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-10">
-          {children}
-        </main>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
